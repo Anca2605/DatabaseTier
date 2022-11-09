66bc0ed69738d69360e2e7da9ce00126201d1e31
Ancaelena2605 <112935366+Ancaelena2605@users.noreply.github.com>
2022-11-09 12:04:23 +0100

    asdf

---
+++ DatabaseTier/DatabaseTier/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace DatabaseTier.Models;
+
+public class Client{
+    [Key]
+    [JsonPropertyName("id")]
+    
+    public string Username { get; set; }
+    public string password { get; set; }
+    
+}
